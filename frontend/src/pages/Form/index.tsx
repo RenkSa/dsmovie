8cb8813706b709bb1357b9f800c79a57ab5c8b2f
RenkSa <renkwiii@outlook.com>
2022-01-10 14:25:17 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form() {
+    
+    return(
+        <h1>Página de Formulário</h1>
+    );
+
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing() {
+    
+    return(
+        <h1>Página de Listagem</h1>
+    );
+
+}
+
+export default Listing;
