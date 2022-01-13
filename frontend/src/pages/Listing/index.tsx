d3d5b3efd71209119de15455c738913f51579758
RenkSa <renkwiii@outlook.com>
2022-01-12 22:37:11 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,33 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
+import MovieStars from "components/MovieStars";
 import Pagination from "components/Pagination";
 
 function Listing() {
-    
-    return(
-        <Pagination />
+
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div><div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div><div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div><div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div><div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 
 }
